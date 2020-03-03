210f67fa8c1a85beee4ab71d10ebbf7b112b4294
DominikIlski <dominik.franciszek.ilski@gmail.com>
2020-03-03 17:58:04 +0100

    Added filePatch reader

---
+++ Algorytm_Ewolucyjny/Models/Town.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorytm_Ewolucyjny.Models
+{
+    class Town
+    {
+        int Numer { set; get; }
+        double X { set; get; }
+        double Y { set; get; }
+
+        public Town(int number, double x, double y)
+        {
+            Numer = number;
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Algorytm_Ewolucyjny/Services/FileService.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Win32;
+
+namespace Algorytm_Ewolucyjny.Services
+{
+    public class FileService
+    {
+
+        public string ChooseFileToOpen()
+        {
+            var filePath = string.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "D:\\gitProjects\\SI_Lab1\\Algorytm_Ewolucyjny\\EA DATA\\";
+            openFileDialog.Filter = "TSP files (*.tsp)|*.tsp|All files (*.*)| *.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if(openFileDialog.ShowDialog().GetValueOrDefault())
+            {
+                filePath =  openFileDialog.FileName;
+            }
+            else
+            {
+                filePath = "Problem With filePath";
+            }
+
+            return filePath;
+
+        }
+
+    }
+}
