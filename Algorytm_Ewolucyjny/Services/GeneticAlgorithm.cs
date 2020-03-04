b51121c31b4480c77fd8a92a24f1851b79a8c864
DominikIlski <dominik.franciszek.ilski@gmail.com>
2020-03-04 22:36:45 +0100

    created classes to geneticAlgorithm

---
+++ Algorytm_Ewolucyjny/Services/GeneticAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorytm_Ewolucyjny.Services
+{
+    class GeneticAlgorithm
+    {
+        int PopSize { set; get; }
+        float Mutation { set; get; }
+
+    }
+}
---
+++ Algorytm_Ewolucyjny/Services/Population.cs
@@ -0,0 +1,30 @@
+﻿using Algorytm_Ewolucyjny.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorytm_Ewolucyjny.Services
+{
+    class Population
+    {
+        int PopSize { set; get; }
+
+        Agglomeration Agglomeration { set; get; }
+
+        public Population(int popSize)
+        {
+            PopSize = popSize;
+            Agglomeration = new Agglomeration();
+        }
+
+
+        public bool SetAgglomeration(Agglomeration agglomeration)
+        {
+
+            Agglomeration = agglomeration ?? new Agglomeration();
+            return !Agglomeration.Name.Equals("Error Creating Agglomeration");
+
+        }
+
+    }
+}
